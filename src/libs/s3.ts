c9abcdc49194ed967701147e490bc3d089bc3773
Danilo Samways <hadeflowing@gmail.com>
2025-06-02 20:31:28 -0300

    fix s3

--- src/libs/s3.ts
+++ src/libs/s3.ts
@@ -3,16 +3,16 @@ import { env } from "../config/env";
 
 export const s3 = new S3Client({
   region: env.AWS_REGION,
-  ...(env.ENVIRONMENT === "development"
-    ? {
-        credentials: {
-          accessKeyId: env.AWS_ACCESS_KEY_ID!,
-          secretAccessKey: env.AWS_SECRET_ACCESS_KEY!,
-        },
-        endpoint: env.AWS_ENDPOINT,
-        forcePathStyle: true,
-      }
-    : undefined),
+  // ...(env.ENVIRONMENT === "development"
+  //   ? {
+  //       credentials: {
+  //         accessKeyId: env.AWS_ACCESS_KEY_ID!,
+  //         secretAccessKey: env.AWS_SECRET_ACCESS_KEY!,
+  //       },
+  //       endpoint: env.AWS_ENDPOINT,
+  //       forcePathStyle: true,
+  //     }
+  //   : undefined),
 });
 
 export interface FileMetadata {
