7fd06dc42121bca8911cf673139bcb5b6a6d03f8
Danilo Samways <hadeflowing@gmail.com>
2025-06-02 18:08:05 -0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ import rateLimit from "express-rate-limit";
 export const createApp = () => {
   const app = express();
 
-  app.set("trust proxy", true);
+  app.set("trust proxy", 1);
 
   // Configuração CORS
   const allowedOrigins = [
